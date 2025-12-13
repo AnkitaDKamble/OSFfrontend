0095d6ebd00e91cc8cda02a23fc40dafff5b36aa
Ankita <ankitakamblemay@gmail.com>
2025-12-13 21:07:21 +0530

    all

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation,useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 const Navbar = () => {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+/*
 import { createRoot } from 'react-dom/client'; // Import createRoot
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // Create a root
-root.render(<App />); // Render your app
+root.render(<App />); // Render your app*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
